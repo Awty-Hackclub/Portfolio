9662d989cddde7810e0b046a8f8eff08e30b7fbb
Tarek Ali <tarekali15@outlook.com>
2020-11-13 11:23:39 -0600

    cleared stuff; were making our navbar from scratch

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
 
 export default function NavBar() {
   return (
-    <Navbar>
-      <Navbar.Brand href="/">[LOGO]</Navbar.Brand>
-      <Navbar.Collapse>
-        <Nav className="Nav">
-          <Nav.Link>Introduction</Nav.Link>
-          <Nav.Link>About Us</Nav.Link>
-          <Nav.Link>Members</Nav.Link>
-          <Nav.Link>Sponsorship</Nav.Link>
-          <Nav.Link>Our Projects</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    
   );
 }
