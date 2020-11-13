f3ef38387e1ccba9909516f74609044295bd83d5
Tarek Ali <tarekali15@outlook.com>
2020-11-13 11:18:56 -0600

    added navbar component; its rendered in App.js; working on it

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <NavBar />
     </div>
   );
 }
--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import 'react-bootstrap';
-
+import React from "react";
+import { Navbar } from "react-bootstrap";
 
 export default function NavBar() {
-    return (
-
-    );
+  return (
+    <Navbar>
+      <Navbar.Brand href="/">[LOGO]</Navbar.Brand>
+      <Navbar.Collapse>
+        <Nav className="Nav">
+          <Nav.Link>Introduction</Nav.Link>
+          <Nav.Link>About Us</Nav.Link>
+          <Nav.Link>Members</Nav.Link>
+          <Nav.Link>Sponsorship</Nav.Link>
+          <Nav.Link>Our Projects</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
