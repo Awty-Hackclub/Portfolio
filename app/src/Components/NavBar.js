621756e711873d4291334d867231f88aff7a6523
Tarek Ali <tarekali15@outlook.com>
2020-11-17 17:01:23 -0600

    mvp for navbar

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    
+    <ul>
+      <li><a>Introduction</a></li>
+      <li><a>About Us</a></li>
+      <li><a>Members</a></li>
+      <li><a>Our Projects</a></li>
+    </ul>
   );
 }
