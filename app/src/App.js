0554e5b062799cc9dd86cf0fa36e7e46fbf3680e
Terence Dumas <watsutx41@gmail.com>
2020-11-17 17:43:07 -0600

    dsajiko

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import Home from './Components/Home'
 import AboutUs from './Components/AboutUs'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
       <AboutUs />
     </div>
   );
---
+++ app/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home(){
+    return(
+        <div>
+            <h2>
+                We are the next generation
+            </h2>
+            <h1>
+                We are the Awty HackClub.
+            </h1>
+        </div>
+    )
+}
