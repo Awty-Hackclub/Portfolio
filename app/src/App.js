1f5418958db1b63f6b805d00505e4395b9850da5
Terence Dumas <watsutx41@gmail.com>
2020-11-17 17:19:55 -0600

    aboutus

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import AboutUs from './Components/AboutUs'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <AboutUs />
     </div>
   );
 }
