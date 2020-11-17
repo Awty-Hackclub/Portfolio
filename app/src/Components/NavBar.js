583e067778a4221a0661b27fe430db7a7992806c
Tarek Ali <tarekali15@outlook.com>
2020-11-17 17:32:53 -0600

    stuff

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -4,10 +4,10 @@ export default function NavBar() {
   return (
     <ul className="Navbar ul">
       <div className="Navbar li">
-        <li><a>Introduction</a></li>
-        <li><a>About Us</a></li>
-        <li><a>Members</a></li>
-        <li><a>Our Projects</a></li>
+        <li><a href="">Introduction</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Members</a></li>
+        <li><a href="">Our Projects</a></li>
       </div>
     </ul>
   );
