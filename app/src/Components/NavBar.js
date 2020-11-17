85e26f1b7b6ec06621953375936b532da0cd5e4c
Tarek Ali <tarekali15@outlook.com>
2020-11-17 17:05:20 -0600

    horizontal alignment

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <ul>
-      <li><a>Introduction</a></li>
-      <li><a>About Us</a></li>
-      <li><a>Members</a></li>
-      <li><a>Our Projects</a></li>
+    <ul className="Navbar ul">
+      <div className="Navbar li">
+        <li><a>Introduction</a></li>
+        <li><a>About Us</a></li>
+        <li><a>Members</a></li>
+        <li><a>Our Projects</a></li>
+      </div>
     </ul>
   );
 }
