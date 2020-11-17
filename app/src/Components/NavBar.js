85dceb645b9b4c15c284f881578fe54d46ae20e9
Terence Dumas <watsutx41@gmail.com>
2020-11-17 17:46:22 -0600

    dsjakd

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -3,9 +3,9 @@ import logo from "../img/logo.svg"
 
 export default function NavBar() {
   return (
-    <ul className="Navbar ul">
-      <img className="Navbar logo" src={logo} alt="logo"></img>
-      <div className="Navbar li">
+    <ul className="Navbar">
+      <img className="logo" src={logo} alt="logo"></img>
+      <div className="Navbar">
         <li><a href="">Introduction</a></li>
         <li><a href="">About Us</a></li>
         <li><a href="">Members</a></li>
