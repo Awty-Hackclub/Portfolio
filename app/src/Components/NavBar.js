76a815a50633b2a0986efb556e3d36099c4cc60d
Terence Dumas <watsutx41@gmail.com>
2020-11-30 18:56:29 -0600

    NAVBAR FOR MFCKIN TAREK

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -3,16 +3,16 @@ import logo from "../img/logo.svg"
 
 export default function NavBar() {
   return (
-    <ul className="Navbar">
+    <div className = "NavBarBox">
       <a href = "./">
         <img className="logo" src={logo} alt="logo"></img>
       </a>
-      <div className = "NavBarText">
+      <ul className="Navbar">
         <li><a href="#home">Home</a></li>
         <li><a href="#aboutus">About Us</a></li>
         <li><a href="#hackathons">Hackathons</a></li>
         <li><a href="#members">Members</a></li>
-      </div>
-    </ul>
+      </ul>
+    </div>
   );
 }
