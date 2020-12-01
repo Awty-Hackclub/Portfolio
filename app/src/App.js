cb66ad755fe6b6401d9ea2354120a3f5b061c0a1
Tarek Ali <tarekali15@outlook.com>
2020-11-30 19:06:37 -0600

    removed imports

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,4 @@
 import "./App.css";
-import NavBar from "./Components/NavBar";
-import Home from './Components/Home'
-import AboutUs from './Components/AboutUs'
-import Hackathons from './Components/Hackathons'
-import Members from './Components/Members'
-import InBetween from './Components/inbetween'
 
 function App() {
   return (
