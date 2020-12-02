2639d781f668bc07c7699a16ed28350b96ac0c2a
Tarek Ali <tarekali15@outlook.com>
2020-12-02 06:18:55 -0600

    added hyperlinks but now need to remove text decoration

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -3,14 +3,12 @@ import React from 'react';
 export default function NavBar() {
     return (
         <div className="navbar">
-            <ul className="navbar-ul">
-                <div className="navbar-li">
-                    <li>About Us</li>
-                    <li>Members</li>
-                    <li>Projects</li>
-                    <li>Support Us</li>
-                </div>
+            <ul>
+                <li><a href="#/">About Us</a></li>
+                <li><a href="#/members">Members</a></li>
+                <li><a href="#/projects">Projects</a></li>
+                <li><a href="#/support">Support Us</a></li>
             </ul>
         </div>
     )
-}
+};
