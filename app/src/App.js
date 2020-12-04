5b01d855aa102fa1f0badd32f64523a6b8f2a8a5
Tarek Ali <tarekali15@outlook.com>
2020-12-03 18:25:12 -0600

    some boilerplate for aboutus styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import AboutUs from "./Components/AboutUs";
 
 function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <AboutUs />
+    </>
   );
 }
 
--- app/src/Components/AboutUs.js
+++ app/src/Components/AboutUs.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function AboutUs(){
-    return(
-        <div>
-            
+export default function AboutUs() {
+    return (
+        <div className="aboutus">
+
         </div>
     )
 }
