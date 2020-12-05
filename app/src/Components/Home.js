a12f796b0abc429dcacf534d77ba0d0d918ed0aa
Terence Dumas <watsutx41@gmail.com>
2020-12-04 18:48:21 -0600

    font

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import home_page_image from '../img/tarek.JPG'
 
 export default function Home() {
     return (
         <div>
             <h1 className="title">We are the Awty Hackclub</h1>
-            <img className = "home_page_image" src = {home_page_image}></img>
         </div>
     )
 }
