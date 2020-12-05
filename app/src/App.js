873621d4564a7c0f990857daab4dbc2e7d63684f
Terence Dumas <watsutx41@gmail.com>
2020-12-04 18:17:00 -0600

    title

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import AboutUs from "./Components/AboutUs";
+import Home from './Components/Home';
 
 function App() {
   return (
     <>
       <NavBar />
       <AboutUs />
+      <Home/>
     </>
   );
 }
--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Typed from 'typed.js';
+
+var options = {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 40
+  };
+  
 
 export default function Home(){
     return(
         <div>
-            
+            <h1 className = "title">We are the Awty Hackclub</h1>
         </div>
     )
 }
