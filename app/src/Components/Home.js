3cf7af7dc1ced408483d1b131c72fbf66767344c
Tarek Ali <tarekali15@outlook.com>
2020-12-04 18:35:00 -0600

    reset hard

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import Typed from 'typed.js';
 
-var options = {
-    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-    typeSpeed: 40
-};
-
-
 export default function Home() {
     return (
         <div>
