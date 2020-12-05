2e609f006526335f1e014380c4f211918197a19f
Terence Dumas <watsutx41@gmail.com>
2020-12-04 18:35:54 -0600

    tarek.jpg

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Typed from 'typed.js';
-
-var options = {
-    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-    typeSpeed: 40
-};
-
+import home_page_image from '../img/tarek.JPG'
 
 export default function Home() {
     return (
         <div>
             <h1 className="title">We are the Awty Hackclub</h1>
+            <img className = "home_page_image" src = {home_page_image}></img>
         </div>
     )
 }
