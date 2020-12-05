f6e14d2aebf0c48dd36c44d72794c6e3eed9c699
Tarek Ali <tarekali15@outlook.com>
2020-12-04 18:20:52 -0600

    3 rem

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -4,13 +4,13 @@ import Typed from 'typed.js';
 var options = {
     strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
     typeSpeed: 40
-  };
-  
+};
 
-export default function Home(){
-    return(
+
+export default function Home() {
+    return (
         <div>
-            <h1 className = "title">We are the Awty Hackclub</h1>
+            <h1 className="title">We are the Awty Hackclub</h1>
         </div>
     )
 }
