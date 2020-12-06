a6c119191f93d1cf4e1ea6c5a29d7eefbee64fb0
Tarek Ali <tarekali15@outlook.com>
2020-12-05 18:43:29 -0600

    formatting?

--- app/src/Components/Hackathons.js
+++ app/src/Components/Hackathons.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Hackathons(){
-    return(
+export default function Hackathons() {
+    return (
         <div>
-            
+
         </div>
     )
 }
