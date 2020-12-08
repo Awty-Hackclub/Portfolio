437c01b8404462608824d3dfc317bd796be9cd03
unknown <gakills12@gmail.com>
2020-12-08 17:59:33 -0600

    lol

--- app/src/Components/AboutUs.js
+++ app/src/Components/AboutUs.js
@@ -1,12 +1,12 @@
 import React from 'react';  
-import Tarek from ''
+import tarek from '../img/tarek.JPG'
 
 export default function AboutUs() {
     return (
         <div className="about_us">
             <h1>About Us</h1>
             <p>We are the Awty International School Hackathon Club (Awty Hackclub). We cultivate new members' skills through side projects; said skills are then implemented in Hackathons. We aim to create a community of young developers and entrepreneurs; leveraging teamwork whilst doing so. Our club provides a medium for students to express creativity, elevate ingenuity, and learn skills that they can implement in their future careers.</p>
-            <img src="/img/tarek.JPG" alt="Tarek Codes"></img>
+            <img src={tarek} alt="Tarek Codes"></img>
         </div>
     )
 }
