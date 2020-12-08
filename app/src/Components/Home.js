9b289d2931faf9932e167899611ae62f0d2f55f5
Tarek Ali <tarekali15@outlook.com>
2020-12-08 17:44:20 -0600

    Support.js

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return (
-        <div>
-            <h1 className="title">We are Awty Hackclub</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="title">We are Awty Hackclub</h1>
+    </div>
+  );
 }
