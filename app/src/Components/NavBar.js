426301312af8637cc38c140e501fa9576dfd2495
Tarek Ali <tarekali@pop-os.localdomain>
2020-12-09 06:32:27 -0600

    formatting

--- app/src/Components/NavBar.js
+++ app/src/Components/NavBar.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export default function NavBar() {
-    return (
-        <div className="navbar" >
-            <ul>
-                <li><a href="#aboutus">About Us</a></li>
-                <li><a href="#members">Members</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#support">Support Us</a></li>
-            </ul>
-        </div>
-    )
-};
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <a href="#aboutus">About Us</a>
+        </li>
+        <li>
+          <a href="#members">Members</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#support">Support Us</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
