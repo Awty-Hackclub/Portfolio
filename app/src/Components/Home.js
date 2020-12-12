050ad62b8c8ec0345230177d8f38faee899a5d14
Tarek Ali <tarekali@pop-os.localdomain>
2020-12-12 12:18:50 -0600

    removed all of our stuff; going to init nextjs instead for convenience

--- app/src/Components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="title">We are Awty Hackclub</h1>
-    </div>
-  );
-}
--- app/src/Components/Members.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Members(){
-    return(
-        <div>
-            
-        </div>
-    )
-}
