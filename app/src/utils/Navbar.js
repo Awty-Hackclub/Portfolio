3a85c1fb3ce3ee9b6e3bd15c81a6ebc84eadf8d4
Tarek Ali <tarekali@pop-os.localdomain>
2020-12-12 14:03:21 -0600

    navbar.js

---
+++ app/src/utils/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <h1>Awty Hackclub</h1>
+            <button></button>
+            <button></button>
+            <hr></hr>
+        </div>
+    )
+}
