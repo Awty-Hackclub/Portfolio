4aeb767c6970933e2a40a398f41f186c29fa989c
Tarek Ali <tarekali@pop-os.localdomain>
2020-12-12 13:44:43 -0600

    html for the navbar. css coming soon

---
+++ app/utils/navbar.js
@@ -0,0 +1,10 @@
+export default Navbar() {
+    return (
+        <div className="navbar">
+            <h1>Awty Hackclub</h1>
+            <button></button>
+            <button></button>
+            <hr></hr>
+        </div>
+    )
+}
