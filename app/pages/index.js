5fa1a4e04d6fb19b81e4739d7f005498f3f82693
Tarek Ali <tarekali@pop-os.localdomain>
2020-12-12 13:46:28 -0600

    index.js invocation of Navbar

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Navbar from '../utils/navbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-  
+    <div>
+      <Navbar />
     </div>
   )
 }
