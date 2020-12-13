53a5fcc5e801f1b4ffc99637aa17fba86e75a317
Tarek Ali <tarekali15@outlook.com>
2020-12-13 11:28:01 -0600

    paragraph in app.js; going to do styles in a minute

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-import Navbar from './utils/Navbar';
+import "./App.css";
+import Navbar from "./utils/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <p>
+        We are the Awty International School Hackathon Club (Awty Hackclub). We
+        cultivate new members' skills through side projects; said skills are
+        then implemented in Hackathons. We aim to create a community of young
+        developers and entrepreneurs; leveraging teamwork whilst doing so. Our
+        club provides a medium for students to express creativity, elevate
+        ingenuity, and learn skills that they can implement in their future
+        careers.
+      </p>
+    </>
   );
 }
 
