625a01f79336356dd71ea176e1b05f92b1dcddd7
Terence <watsutx41@gmail.com>
2020-12-14 19:34:36 -0600

    HomePage

--- app/src/App.js
+++ app/src/App.js
@@ -5,15 +5,23 @@ function App() {
   return (
     <>
       <Navbar />
-      <p>
-        We are the Awty International School Hackathon Club (Awty Hackclub). We
-        cultivate new members' skills through side projects; said skills are
-        then implemented in Hackathons. We aim to create a community of young
-        developers and entrepreneurs; leveraging teamwork whilst doing so. Our
-        club provides a medium for students to express creativity, elevate
-        ingenuity, and learn skills that they can implement in their future
-        careers.
-      </p>
+      <table className = "content">
+        <tr>
+          <th className = "paragraph">
+            We are the Awty International School Hackathon Club (Awty Hackclub). We
+            cultivate new members' skills through side projects; said skills are
+            then implemented in Hackathons. We aim to create a community of young
+            developers and entrepreneurs; leveraging teamwork whilst doing so. Our
+            club provides a medium for students to express creativity, elevate
+            ingenuity, and learn skills that they can implement in their future
+            careers.
+          </th>
+          <th className = "slideShow">
+
+          </th>
+        </tr>
+
+      </table>
     </>
   );
 }
