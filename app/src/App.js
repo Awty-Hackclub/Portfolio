8768b4d0cb347f857442ddd2ef045087f8381dec
Tarek Ali <tarekali15@outlook.com>
2020-12-14 19:43:44 -0600

    formatting

--- app/src/App.js
+++ app/src/App.js
@@ -5,22 +5,19 @@ function App() {
   return (
     <>
       <Navbar />
-      <table className = "content">
+      <table className="content">
         <tr>
-          <th className = "paragraph">
-            We are the Awty International School Hackathon Club (Awty Hackclub). We
-            cultivate new members' skills through side projects; said skills are
-            then implemented in Hackathons. We aim to create a community of young
-            developers and entrepreneurs; leveraging teamwork whilst doing so. Our
-            club provides a medium for students to express creativity, elevate
-            ingenuity, and learn skills that they can implement in their future
-            careers.
+          <th className="paragraph">
+            We are the Awty International School Hackathon Club (Awty Hackclub).
+            We cultivate new members' skills through side projects; said skills
+            are then implemented in Hackathons. We aim to create a community of
+            young developers and entrepreneurs; leveraging teamwork whilst doing
+            so. Our club provides a medium for students to express creativity,
+            elevate ingenuity, and learn skills that they can implement in their
+            future careers.
           </th>
-          <th className = "slideShow">
-
-          </th>
+          <th className="slideShow"></th>
         </tr>
-
       </table>
     </>
   );
