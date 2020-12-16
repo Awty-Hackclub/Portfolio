50c5664970b3c8c16937663a7f0c40d88c89084d
Tarek Ali <tarekali15@outlook.com>
2020-12-16 17:48:02 -0600

    hyperlinks

--- app/src/utils/Navbar.js
+++ app/src/utils/Navbar.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <ul>
-                <li><a className = "AwtyHackclub" href = "./"><h1 className = 'AwtyHackclub'>Awty Hackclub</h1></a></li>
-                <li><button>Projects</button></li>
-                <li><button>Support Us</button></li>
+                <Link to="/"><li><a className = "AwtyHackclub" href = "./"><h1 className = 'AwtyHackclub'>Awty Hackclub</h1></a></li></Link>
+                <Link to="/projects"><li><button>Projects</button></li></Link>
+                <Link to="/support"><li><button>Support Us</button></li></Link>
             </ul>
             <hr></hr>
         </div>
