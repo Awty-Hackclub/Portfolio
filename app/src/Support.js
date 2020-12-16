cc3a077aafd5507eeab1be236438bf3cc3dedb9e
Tarek Ali <tarekali15@outlook.com>
2020-12-16 17:35:33 -0600

    support.js

--- app/src/Support.js
+++ app/src/Support.js
@@ -1,2 +1,14 @@
+import { Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./utils/Navbar";
+
+export default Support() {
+    return (
+        <>
+            <Navbar />
+            <div className="svg-grid">
+            
+            </div>
+        </>
+    );
+}
