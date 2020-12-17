013890ac7822eae56bfc1275a3e0dbc61cb0babc
Terence Dumas <watsutx41@gmail.com>
2020-12-17 17:15:21 -0600

    projects

---
+++ app/src/Projects.js
@@ -0,0 +1,14 @@
+import { Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./utils/Navbar";
+
+export default function Projects() {
+    return (
+        <>
+            <Navbar />
+            <div className="svg-grid">
+
+            </div>
+        </>
+    );
+}
