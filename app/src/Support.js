4b0c4e508fc438f67a93780e0c721e3dceacbfac
Terence <watsutx41@gmail.com>
2020-12-19 12:29:11 +0100

    support

--- app/src/Support.js
+++ app/src/Support.js
@@ -1,20 +1,20 @@
 import { Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./utils/Navbar";
-import Twitter from "./icons/twitter.svg";
-import Github from "./icons/github.svg";
-import Instagram from "./icons/instagram.svg";
-import Youtube from "./icons/youtube.svg";
+import Twitter from "./icons/twitter.jsx";
+import Github from "./icons/github.jsx";
+import Instagram from "./icons/instagram.jsx";
+import Youtube from "./icons/youtube.jsx";
 
 export default function Support() {
     return (
         <>
             <Navbar />
             <div className="svg-grid">
-                <img src = {Twitter} className = "Twitter"></img>
-                <img src = {Github} className = "Github"></img>
-                <img src = {Instagram} className = "Instagram"></img>
-                <img src = {Youtube} className = "Youtube"></img>
+                <div className = "circle"><Twitter class = "icon"/></div>
+                <div className = "circle"><Github class = "icon"/></div>
+                <div className = "circle"><Instagram class = "icon"/></div>
+                <div className = "circle"><Youtube class = "icon"/></div>
             </div>
         </>
     );
